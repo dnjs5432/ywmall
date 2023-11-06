e8339869c1fe3ecb9a82d013ed4dfc3014287bfb
yewon <dnjs5432@naver.com>
2023-11-06 20:31:48 +0900

    최종의 최종의 최종의

--- schemas/index.js
+++ schemas/index.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const connect = () => {
   mongoose
-    .connect('mongodb+srv://dnjs5432:wldkskfn3@leeyewon.vuqafjp.mongodb.net/', {
-      dbName: 'node_lv1',
-    })
+    .connect(process.env.DB_URL, { dbName: 'node_lv1' })
     .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
